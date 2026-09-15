2f134684d2462926552622461a3803ebfb808b9e
trinhtanphat <trinhtanphat0000@gmail.com>
2026-09-15 07:37:04 +0700

    recovery: promote dependency-closed Game.Logic types

---
+++ Game.Logic/Cmd/MoveStopCommand.cs
@@ -0,0 +1,14 @@
+using Game.Base.Packets;
+using Game.Logic.Phy.Object;
+
+namespace Game.Logic.Cmd
+{
+[GameCommand(10, "开始移动")]
+public class MoveStopCommand : ICommandHandler
+{
+	public void HandleCommand(BaseGame game, Player player, GSPacketIn packet)
+	{
+	}
+}
+
+}
---
+++ Game.Logic/eMap.cs
@@ -0,0 +1,11 @@
+namespace Game.Logic
+{
+public enum eMap
+{
+	ConsortiaBoss = 1291,
+	FightFootballTime = 1313,
+	NPCEnemies = 105,
+	CatchBeast = 1347
+}
+
+}
